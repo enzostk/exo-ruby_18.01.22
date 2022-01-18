91e82b03aaa4171be5a3ed1227879879a2726342
enzostk <enzostk@gmail.com>
2022-01-18 20:01:38 +0100

    last commit

--- exo_16.rb
+++ exo_16.rb
@@ -4,6 +4,7 @@ etage = gets.chomp.to_i
 x = 1
 y = etage-1
 
+
 while etage > 25 || etage < 1 
     puts "Il doit y avoir entre 1 et 25 étages !"
 end
@@ -12,12 +13,12 @@ puts "Voici la pyramide:"
 
 etage.times do
     y.times do
-        print " "
-      end
-     y -= 1
+            print " "
+            end
+            y -= 1
     x.times do
-        print"#"
-    end
+            print"#"
+            end
     x += 1
     puts " "
 end
--- exo_17.rb
+++ exo_17.rb
@@ -1,1 +1,30 @@
-a
+puts "Salut, bienvenue dans ma super pyramide! Combien d'étages veux-tu?"
+print "> "
+etage = gets.chomp.to_i
+x = 1
+y = etage-1
+z = etage-1
+
+while etage > 25 || etage < 1 
+    puts "Il doit y avoir entre 1 et 25 étages !"
+end
+
+puts "Voici la pyramide:"
+
+etage.times do
+    y.times do
+            print " "
+            end
+            y -= 1
+    
+    x.times do
+            print"#"
+            end
+            x += 2
+           
+    z.times do
+            print " "
+            end 
+            z -= 1
+            puts " "
+end
